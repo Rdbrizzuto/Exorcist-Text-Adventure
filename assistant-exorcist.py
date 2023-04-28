d7c36a5bf29968040ee05c36d9f704a8ec80e5c1
Rdbrizzuto <123248855+Rdbrizzuto@users.noreply.github.com>
2023-04-28 17:35:20 -0400

    updated game so it is fully playable

--- assistant-exorcist.py
+++ assistant-exorcist.py
@@ -2,69 +2,68 @@ import random
 
 def main():
     welcome()
-    print("HOUSE LAYOUT (as drawing??) and what room you are in\n")
+    print("HOUSE LAYOUT (as drawing??)\nYou are currently in the living room\n")
 
     class Ghost:
-        def __init__(self, EMF, UVlight, temp, pic):
+        def __init__(self, EMF, UVlight, temp, pic, name):
             self.EMF = EMF
             self.UVlight = UVlight
             self.temp = temp
             self.pic = pic
+            self.name = name
 
 # defines all the types of ghosts that can haunt the house, and randomly selects one for the game
-    wraith = Ghost(True, False, True, False)
-    vengeful_spirit = Ghost(True, True, False, False)
-    demon = Ghost(True, False, False, True)
-    shade = Ghost(False, True, True, False)
-    specter = Ghost(False, False, True, True)
-    poltergeist = Ghost(False, True, False, True)
+    wraith = Ghost(True, False, True, False, "wraith")
+    vengeful_spirit = Ghost(True, True, False, False, "vengeful spirit")
+    demon = Ghost(True, False, False, True, "demon")
+    shade = Ghost(False, True, True, False, "shade")
+    specter = Ghost(False, False, True, True, "specter")
+    poltergeist = Ghost(False, True, False, True, "poltergeist")
 
     ghost_types = [wraith, vengeful_spirit, demon, shade, specter, poltergeist]
 
-# selects the ghost and haunted room for the game. Also starts the actual playing of the game
+# selects the ghost and haunted room for the game, then starts the actual game
     current_ghost = random.choice(ghost_types)
     haunted_room = pick_room()
-
     playing(current_ghost, haunted_room)
 
 # this is where you guess what the type of ghost is, and find out if you are wrong or right
 
-    print("What type of ghost do you think is haunting this house?")
-    input_guess = input("What type of ghost is haunting this house?\n1. Wraith\n2. Vengeful spirit\n3. Demon\n4. Shade\n5. Specter\n6. Shade")
+    input_guess = input("What type of ghost is haunting this house?\n1. Wraith\n2. Vengeful spirit\n3. Demon\n4. Shade\n5. Specter\n6. Poltergeist\n\n")
 
     while True:
         if input_guess == "1":
-            actual_guess = wraith
+            actual_guess = "wraith"
             break
         
         elif input_guess == "2":
-            actual_guess = vengeful_spirit
+            actual_guess = "vengeful spirit"
             break
 
         elif input_guess == "3":
-            actual_guess = demon
+            actual_guess = "demon"
             break
 
-        elif input_guess == "2":
-            actual_guess = shade
+        elif input_guess == "4":
+            actual_guess = "shade"
             break
 
-        elif input_guess == "2":
-            actual_guess = specter
+        elif input_guess == "5":
+            actual_guess = "specter"
             break
 
-        elif input_guess == "2":
-            actual_guess = poltergeist
+        elif input_guess == "6":
+            actual_guess = "poltergeist"
             break
 
         else:
             ("Please enter a valid response")
 
-    if actual_guess == current_ghost.self:
+    if actual_guess == current_ghost.name:
         print("Correct! Your boss is going to be so proud")
 
     else:
-        print("Wrong! Better start looking for a new job...")
+        print(f"Wrong! It was a {current_ghost.name}. Better start looking for a new job...")
 
 
 
@@ -87,7 +86,7 @@ def welcome():
 # explains the rules of the game and how it is played
 def explain_game():
     print("EXPLANATION\n")
-    input('Press "Enter" to continue')
+    input('Press "Enter" to continue\n')
 
 
 # randomly picks which room the ghost will haunt
@@ -100,7 +99,7 @@ def pick_room():
 # which rooms can you move into from the room you are in
 def change_rooms(current_room):
     if current_room == "living room":
-        choice = input("Which room would you like to move into?\n1. Bedroom\n2. Dining room\n3. Garage")
+        choice = input("Which room would you like to move into?\n1. Bedroom\n2. Dining room\n3. Garage\n\n")
         while True:
             if choice == "1":
                 current_room = "bedroom"
@@ -118,7 +117,7 @@ def change_rooms(current_room):
                 ("Please enter a valid response")
 
     elif current_room == "bedroom":
-         choice = input("Which room would you like to move into?\n1. Bathroom\n2. Living room")
+         choice = input("Which room would you like to move into?\n1. Bathroom\n2. Living room\n\n")
          while True:
             if choice == "1":
                 current_room = "bathroom"
@@ -132,7 +131,7 @@ def change_rooms(current_room):
                 ("Please enter a valid response")
 
     elif current_room == "bathroom":
-         choice = input("Which room would you like to move into?\n1. Bedroom")
+         choice = input("Which room would you like to move into?\n1. Bedroom\n\n")
          while True:
             if choice == "1":
                 current_room = "bedroom"
@@ -143,7 +142,7 @@ def change_rooms(current_room):
 
 
     elif current_room == "dining room":
-        choice = input("Which room would you like to move into?\n1. Kitchen\n2. Garage\n3. Living room")
+        choice = input("Which room would you like to move into?\n1. Kitchen\n2. Garage\n3. Living room\n\n")
         while True:
             if choice == "1":
                 current_room = "kitchen"
@@ -161,7 +160,7 @@ def change_rooms(current_room):
                 ("Please enter a valid response") 
 
     elif current_room == "kitchen":
-        choice = input("Which room would you like to move into?\n1. Dining room")
+        choice = input("Which room would you like to move into?\n1. Dining room\n\n")
         while True:
             if choice == "1":
                 current_room = "dining room"
@@ -171,7 +170,7 @@ def change_rooms(current_room):
                 ("Please enter a valid response") 
 
     elif current_room == "garage":
-        choice = input("Which room would you like to move into?\n1. Living room\n2. Dining room")
+        choice = input("Which room would you like to move into?\n1. Living room\n2. Dining room\n\n")
         while True:
             if choice == "1":
                 current_room = "living room"
@@ -190,55 +189,53 @@ def change_rooms(current_room):
 
 # the choices you can make playing (Move, Ghost type checks, Consult diary, Guess)
 def playing(current_ghost, haunted_room):
-    time_left = 120
+    time_left = 150
     current_room = "living room"
 
     while time_left > 0:
-        choice = input("What do you want to do?\n1.Move to a different room\n2.Check room's EMF\n3. Shine UV light around room\n4. Check room's temperature\n5. Take a photo of the room\n6. Consult Paranormal Encyclopedia\n7. Guess ghost")
+        choice = input("What do you want to do?\n1. Move to a different room\n2. Check room's EMF\n3. Shine UV light around room\n4. Check room's temperature\n5. Take a photo of the room\n6. Consult Paranormal Encyclopedia\n7. Guess ghost\n\n")
 
         # moving to a different location
         if choice == "1":
             current_room = change_rooms(current_room)
-            print(f"You have entered the {current_room}")
-            time_left = time_left - 10
+            print(f"You have entered the {current_room}\n")
         
         # check EMF
         elif choice == "2":
             if (current_room == haunted_room) and (current_ghost.EMF == True):
-                print("The EMF reader is going crazy!")
+                print("The EMF reader is going crazy!\n")
             else:
-                print("Barely even a beep from your EMF reader...")
+                print("Barely even a beep from your EMF reader...\n")
             time_left = time_left - 10
 
         # check for fingerprints with UV
         elif choice == "3":
             if (current_room == haunted_room) and (current_ghost.UVlight == True):
-                print("As you shine your UV light on the wall next to you, you see the outline of a hand")
+                print("As you shine your UV light on the wall next to you, you see the outline of a hand\n")
             else:
-                print("You shine the ligh all over but don't see anything out of place")
+                print("You shine the light all over, but don't see anything out of place\n")
             time_left = time_left - 10
 
         # check temperature of room
         elif choice == "4":
             if (current_room == haunted_room) and (current_ghost.temp == True):
-                print("Your thermometer reads zero degrees...")
+                print("Your thermometer reads zero degrees...\n")
             else:
-                print("Its a normal room temp in here")
+                print("Its a normal room temp in here\n")
             time_left = time_left - 10
         
         # take photo of the room
         elif choice == "5":
             if (current_room == haunted_room) and (current_ghost.pic == True):
-                print("As you look at the photo that you just took, you notice a silhouette crouching in the corner!")
+                print("As you look at the photo that you just took, you notice a silhouette crouching in the corner!\n")
             else:
-                print("You can't find anything out of place in the photo you took")
+                print("You can't find anything out of place in the photo you took\n")
             time_left = time_left - 10
 
         # consult encyclopedia
         elif choice == "6":
             print("ENCYCLOPEDIA")
-            input('Press "Enter" to continue')
-            break
+            input('Press "Enter" to continue\n')
 
         # guess ghost
         elif choice == "7":
